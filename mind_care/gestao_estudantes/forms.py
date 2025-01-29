e75e8731e15e1b965440d54804817e861cda4cf7
dlcantanhede <blackdl2021@gmail.com>
2025-01-29 06:29:56 -0300

    Commit inicial

---
+++ mind_care/gestao_estudantes/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Address, Organization, Student, City
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = ['street', 'number', 'neighborhood', 'cep', 'complement', 'city', 'active']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Popula o campo de cidade com as cidades existentes
+        self.fields['city'].queryset = City.objects.all()
+        
+
+class OrganizationForm(forms.ModelForm):
+    class Meta:
+        model = Organization
+        fields = ['name', 'phone', 'email', 'active']
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['code', 'name', 'phone', 'email', 'gender', 'photo', 'organization', 'active']
+        
